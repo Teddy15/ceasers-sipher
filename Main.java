d27b9615246cf22d67caac871d16973cfce23319
Teodora Yovcheva <t.k.yovcheva@gmail.com>
2022-12-15 14:32:03 +0200

    Final version
    Final version

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     private static final int OFFSET = 3;
     private static final String QUIT_LETTER = "Q";
+    private static final String CYPHER_LETTER = "C";
+    private static final String DECIPHER_LETTER = "D";
     private static final char FIRST_LETTER = 'A';
     private static final char LAST_LETTER = 'Z';
     private static final int FIRST_LETTER_CODE = 65;
@@ -45,18 +47,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Press q or Q to exit!");
-
+        System.out.println("Press q or Q to exit, c/C to cypher, d/D to decipher!");
         while(in.hasNextLine()) {
             String s = in.nextLine().toUpperCase();
             if (QUIT_LETTER.equalsIgnoreCase(s)) {
                 break;
             }
-            System.out.println("You entered: " + s);
-            String e = encryptMessage(s);
-            System.out.println("Encrypted message: " + e);
-            String d = decryptMessage(e);
-            System.out.println("Decrypted message: " + d);
+
+            System.out.println("Enter your message below!");
+            String a = in.nextLine().toUpperCase();
+            System.out.println("You entered: " + a);
+
+            if (CYPHER_LETTER.equalsIgnoreCase(s)) {
+                System.out.println("Encrypted message: " + encryptMessage(a));
+            }
+            if (DECIPHER_LETTER.equalsIgnoreCase(s)) {
+                System.out.println("Encrypted message: " + decryptMessage(a));
+            }
+            System.out.println("Press q or Q to exit, c/C to cypher, d/D to decypher!");
         }
 
         in.close();
